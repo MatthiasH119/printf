c842380f9ded6bd5e062dd928429a2eaa3e89d97
MatthiasH119 <matthiasheiming@aol.de>
2022-05-14 10:33:55 +0200

    ASCII-Art; Aufgabe_1.8

---
+++ ASCII-Art.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main () {
+	printf("    :''._..---.._.'';\n");
+	printf("    `.             .'\n");
+	printf("    .'    ^   ^    `.\n");
+	printf("   :      a   a      :                 __....._\n");
+	printf("   :     _.-0-._     :---'''''-....--''        '.\n");
+	printf("    :  .'   :   `.  :                          `,`.\n");
+	printf("     `.: '--'--' :.'                             ; ;\n");
+	printf("      : `._`-'_.'                                ;.'\n");
+	printf("      `.   '''                                   ;\n");
+	printf("       `.               '                        ;\n");
+	printf("        `.     `        :           `            ;\n");
+	printf("         .`.    ;       ;           :           ;\n");
+	printf("       .'    `-.'      ;            :          ;`.\n");
+	printf("   __.'      .'      .'              :        ;   `.\n");
+	printf(" .'      __.'      .'`--..__      _._.'      ;      ;\n");
+	printf(" `......'        .'         `''''`.'        ;......-'\n");
+	printf("       `.......-'                 `........'\n");
+	
+	printf("\nArt by Joan G. Stark\n");
+
+	return 0;
+}
